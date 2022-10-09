8974fed0a296bfc972711f73ec031db85cf725a6
viktorstrate <viktorstrate@gmail.com>
2022-10-09 18:14:24 +0200

    remove ELK experimentations

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,39 +13,3 @@ resource "helm_release" "rabbitmq" {
   chart      = "rabbitmq"
   version    = "10.3.9"
 }
-
-resource "helm_release" "elastic" {
-  name             = "elk-elastic"
-  namespace        = "monitoring"
-  create_namespace = true
-
-  repository = "https://helm.elastic.co"
-  chart      = "elasticsearch"
-
-  values = ["${file("elk-elastic.yaml")}"]
-}
-
-resource "helm_release" "kibana" {
-  name             = "elk-kibana"
-  namespace        = "monitoring"
-  create_namespace = true
-  depends_on       = [helm_release.elastic]
-
-  repository = "https://helm.elastic.co"
-  chart      = "kibana"
-
-  values = ["${file("elk-kibana.yaml")}"]
-}
-
-resource "helm_release" "logstash" {
-  name             = "elk-logstash"
-  namespace        = "monitoring"
-  create_namespace = true
-  depends_on       = [helm_release.elastic]
-
-  repository = "https://helm.elastic.co"
-  chart      = "logstash"
-
-  values = ["${file("elk-logstash.yaml")}"]
-}
-
