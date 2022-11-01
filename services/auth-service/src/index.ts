acfab4ac781550b8c5f4cedb939723eb9e70280e
viktorstrate <viktorstrate@gmail.com>
2022-11-01 14:06:56 +0100

    integrate barebone frontend by Jonas

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -3,12 +3,20 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
+app.get('/api/v1/auth', (req, res) => {
   res.send('This is the API service')
 })
 
 const server = app.listen(port, () => {
   console.log(`Authentication service listening on port ${port}`)
+})
+
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .send(
+      `404 auth service route '${req.path}' not found, note service is served under /api/v1/auth`
+    )
 })
 
 process.on('SIGTERM', () => {
