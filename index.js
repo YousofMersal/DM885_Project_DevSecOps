cf948371b577024b0b582bf78a4c170b5fde650b
Dannikiskov <114243245+Dannikiskov@users.noreply.github.com>
2022-11-06 16:15:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
+function please(){
+    return "Hello, World!"
+}
 
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT;
-
-app.get('/', function (req, res) {
-  res.json({ id: 0, name: `test` });
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+module.exports = {
+    please: function pleaseInner(){
+        return please()
+    }
+}
