e3dad4052c863948088d7aab1db617f4df44bdfe
Dannikiskov <114243245+Dannikiskov@users.noreply.github.com>
2022-11-18 12:20:16 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,13 @@ provider "kubectl" {
   config_path = "~/.kube/config"
 }
 
+resource "helm_release" "solvers-postgres" {
+  name       = "solvers-postgres"     
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "postgresql"
+
+}
+
 # istio service mesh used by knative
 resource "helm_release" "istio-base" {
   name       = "istio"
