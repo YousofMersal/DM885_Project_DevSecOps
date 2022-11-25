9944aa5b132c37f6fc931f488cd58bdcf1ebbef6
Henrik <henrik>
2022-11-25 20:22:17 +0100

    adding resources

--- terraform_gke/gke.tf
+++ terraform_gke/gke.tf
@@ -57,3 +57,78 @@ resource "google_container_node_pool" "primary_nodes" {
     }
   }
 }
+
+
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubectl" {
+  config_path = "~/.kube/config"
+}
+
+# istio service mesh used by knative
+resource "helm_release" "istio-base" {
+  name       = "istio"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "base"
+
+  namespace        = "istio-system"
+  create_namespace = true
+}
+
+resource "helm_release" "istio-discovery" {
+  depends_on = [
+    helm_release.istio-base
+  ]
+
+  name       = "istiod"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "istiod"
+
+  namespace        = "istio-system"
+  create_namespace = true
+}
+
+resource "helm_release" "istio-ingressgateway" {
+  depends_on = [
+    helm_release.istio-discovery,
+  ]
+
+  name       = "istio-ingressgateway"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "gateway"
+
+  namespace        = "istio-system"
+  create_namespace = true
+}
+
+# https://registry.terraform.io/providers/gavinbunney/kubectl/latest/docs/data-sources/kubectl_path_documents
+# enable ease of splitting multi-document yaml content, from a collection of matching files
+data "kubectl_path_documents" "knative-yamls" {
+  pattern = "./knative/*.yaml"
+}
+
+# "kubectl_manifest" - Create a Kubernetes resource using raw YAML manifests.
+resource "kubectl_manifest" "knative-operator" {
+  depends_on = [
+    helm_release.istio-discovery,
+    helm_release.istio-ingressgateway
+  ]
+
+  for_each  = toset(data.kubectl_path_documents.knative-yamls.documents)
+  yaml_body = each.value
+}
+
+resource "helm_release" "prometheus" {
+  name = "prometheus"
+  chart = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+}
