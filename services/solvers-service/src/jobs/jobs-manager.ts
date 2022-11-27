2c15575731a02b455164dbdd7abe78c2f950f15e
viktorstrate <viktorstrate@gmail.com>
2022-11-27 15:24:59 +0100

    Add first functional job scheduler

--- services/solvers-service/src/jobs/jobs-manager.ts
+++ services/solvers-service/src/jobs/jobs-manager.ts
@@ -1,4 +1,5 @@
 import k8s from '@kubernetes/client-node'
+import { registerJobWatch } from './job-watcher.js'
 import { K8sClient } from './k8s-client.js'
 
 export interface SolverJobDescription {
@@ -7,10 +8,16 @@ export interface SolverJobDescription {
   mznData?: string
 }
 
+export interface SolverJob {
+  id: string
+  job: k8s.V1Job
+  configMap: k8s.V1ConfigMap
+}
+
 export async function startJob(
   client: K8sClient,
   jobDesc: SolverJobDescription
-) {
+): Promise<SolverJob> {
   const configMapName = `minizinc-job-data-${jobDesc.id}`
   const jobName = `minizinc-job-${jobDesc.id}`
 
@@ -26,6 +33,24 @@ export async function startJob(
   if (jobDesc.mznData) map.data.data = jobDesc.mznData
 
   map = (await client.core.createNamespacedConfigMap(client.ns, map)).body
+
+  const commandArgs = [
+    'minizinc',
+    '-m',
+    '/tmp/mzn-model/model.mzn',
+    '--output-mode',
+    'json',
+    '--json-stream', // stream result as newline-separated json objects
+    '--intermediate-solutions', // print intermediate solutions
+    '--statistics', // print statistics when the solver finishes
+    '--output-time', // add output time to json objects
+    '--output-objective',
+    '--output-output-item',
+  ]
+
+  if (jobDesc.mznData != null) {
+    commandArgs.push('-d', '/tmp/mzn-model/model.dzn')
+  }
 
   // https://kubernetes.io/docs/concepts/workloads/controllers/job/#running-an-example-job
   let job = new k8s.V1Job()
@@ -44,12 +69,7 @@ export async function startJob(
           {
             name: 'minizinc-solver',
             image: 'minizinc/minizinc:latest',
-            command: [
-              'minizinc',
-              '/tmp/mzn-model/model.mzn',
-              '--output-mode',
-              'json',
-            ],
+            command: commandArgs,
             volumeMounts: [
               {
                 name: 'mzn-model',
@@ -84,8 +104,20 @@ export async function startJob(
     throw err
   }
 
-  return {
+  const jobResult = {
+    id: jobDesc.id,
     job,
     configMap: map,
   }
+
+  registerJobWatch(client, jobResult)
+    .catch((err) => {
+      console.warn('WARN: failed to watch job', err)
+    })
+    .finally(() => {
+      console.log(`Cleaning up config map for job: ${jobDesc.id}`)
+      client.core.deleteNamespacedConfigMap(configMapName, client.ns)
+    })
+
+  return jobResult
 }
