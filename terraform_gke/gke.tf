af86edde3477e3e21bf66e5d80e11b2861e2415d
Henrik <henrik>
2022-11-30 11:57:18 +0100

    cleaning

--- terraform_gke/gke.tf
+++ terraform_gke/gke.tf
@@ -22,15 +22,7 @@ variable "gke_node_machine_type" {
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.zone
-   
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  # remove_default_node_pool = false
-  initial_node_count       = 2
-
-  # network    = google_compute_network.vpc.name
-  # subnetwork = google_compute_subnetwork.subnet.name
+  initial_node_count = var.gke_num_nodes
 
   node_config {
     machine_type = var.gke_node_machine_type
@@ -43,33 +35,6 @@ resource "google_container_cluster" "primary" {
     ]
   }
 }
-
-# Separately Managed Node Pool
-# resource "google_container_node_pool" "primary_nodes" {
-#   name       = "${google_container_cluster.primary.name}"
-#   location   = var.zone
-#   cluster    = google_container_cluster.primary.name
-#   node_count = var.gke_num_nodes
-
-#   node_config {
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#     ]
-
-#     labels = {
-#       env = var.project_id
-#     }
-
-#     machine_type = var.gke_node_machine_type
-#     tags         = ["gke-node", "${var.project_id}-gke"]
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
-#   }
-# }
-
-
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
@@ -138,8 +103,8 @@ resource "kubectl_manifest" "knative-operator" {
   yaml_body = each.value
 }
 
-# resource "helm_release" "prometheus" {
-#   name = "prometheus"
-#   chart = "kube-prometheus-stack"
-#   repository = "https://prometheus-community.github.io/helm-charts"
-# }
+resource "helm_release" "prometheus" {
+  name = "prometheus"
+  chart = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+}
--- terraform_gke/vpc.tf
+++ terraform_gke/vpc.tf
@@ -15,17 +15,3 @@ provider "google" {
   region  = var.region
   zone = var.zone
 }
-
-# # VPC
-# resource "google_compute_network" "vpc" {
-#   name                    = "${var.project_id}-vpc"
-#   auto_create_subnetworks = "false"
-# }
-
-# # Subnet
-# resource "google_compute_subnetwork" "subnet" {
-#   name          = "${var.project_id}-subnet"
-#   region        = var.region
-#   network       = google_compute_network.vpc.name
-#   ip_cidr_range = "10.10.0.0/24"
-# }
