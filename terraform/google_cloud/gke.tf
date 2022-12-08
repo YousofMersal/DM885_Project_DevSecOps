ba8a25628209c4f1bf57c6f9af93643797ae43c1
Henrik <henrik>
2022-12-08 16:25:26 +0100

    removed autopilot

--- terraform/google_cloud/gke.tf
+++ terraform/google_cloud/gke.tf
@@ -26,50 +26,37 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  # remove_default_node_pool = true
-  # initial_node_count       = 1
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
-
-  ip_allocation_policy {
-  }
-
-  private_cluster_config {
-    enable_private_nodes = true
-    enable_private_endpoint = false
-  }
-
-  enable_autopilot = true
 }
 
-# # Separately Managed Node Pool
-# resource "google_container_node_pool" "primary_nodes" {
-#   name       = google_container_cluster.primary.name
-#   location   = var.zone
-#   cluster    = google_container_cluster.primary.name
-#   node_count = var.gke_num_nodes
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
 
-#   node_config {
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/compute",
-#       "https://www.googleapis.com/auth/devstorage.read_only",
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#     ]
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
 
-#     labels = {
-#       env = var.project_id
-#     }
+    labels = {
+      env = var.project_id
+    }
 
-#     machine_type = var.gke_node_machine_type
-#     disk_type = "pd-ssd"
-#     tags         = ["gke-node", "${var.project_id}-gke"]
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
-#   }
-# }
-
-
-
+    machine_type = var.gke_node_machine_type
+    disk_type = "pd-ssd"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
