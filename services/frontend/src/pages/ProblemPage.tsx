cc582bf9262c76c1723a3a7e35d8fe8208020da0
jonasgroendahl <jonas.groendahlxd@gmail.com>
2022-12-11 18:03:13 +0100

    wire auth and solvers service, auth docker image change, timeout change

--- services/frontend/src/pages/ProblemPage.tsx
+++ services/frontend/src/pages/ProblemPage.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { apiStartJob } from "../request";
 
-interface IProblemPageProps {}
+export const ProblemPage: React.FC = () => {
+  const handleStartJob = async () => {
+    try {
+      const response = await apiStartJob();
+      console.log("handleStartJob success", response);
+    } catch (e) {
+      console.log("handleStartJob fail", e);
+    }
+  };
 
-export const ProblemPage: React.FC<IProblemPageProps> = () => {
   return (
     <div>
       <div>I am a problem </div>
+      <button onClick={handleStartJob}>Start job</button>
     </div>
   );
 };
