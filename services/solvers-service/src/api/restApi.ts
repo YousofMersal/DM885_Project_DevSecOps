b9744eade26768a846db061468a94face9d753e1
viktorstrate <viktorstrate@gmail.com>
2022-12-19 13:57:26 +0100

    Add JWT authentication for all routes for solver service

--- services/solvers-service/src/api/restApi.ts
+++ services/solvers-service/src/api/restApi.ts
@@ -1,5 +1,5 @@
 import { K8sClient } from './../jobs/k8s-client.js'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import os from 'os'
 
 import jobsRoute from './jobsRoute.js'
@@ -7,6 +7,7 @@ import solversRoute from './solversRoute.js'
 import modelsRoute from './modelsRoute.js'
 import type { Client } from 'pg'
 import cors from 'cors'
+import { jwtMiddleware } from '../jwt.js'
 
 function startRestAPI(k8sClient: K8sClient, db: Client) {
   const app = express()
@@ -14,6 +15,7 @@ function startRestAPI(k8sClient: K8sClient, db: Client) {
 
   app.use(express.json())
   app.use(cors())
+  app.use(jwtMiddleware())
 
   app.use('/api/v1/jobs', jobsRoute(k8sClient, db))
   app.use('/api/v1/solvers', solversRoute(db))
@@ -23,6 +25,11 @@ function startRestAPI(k8sClient: K8sClient, db: Client) {
     console.log(
       `Solvers service started at http://${os.hostname ?? 'localhost'}:${port}`
     )
+  })
+
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error('Internal server error: ', err)
+    res.status(((err as any).status as number) ?? 500).send(err)
   })
 
   return server
