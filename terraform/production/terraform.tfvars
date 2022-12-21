c43803b26a5418f4e220acb6dc4e7de74cf5c444
Henrik <henrikrossen@users.noreply.github.com>
2022-12-21 12:29:15 +0100

    Allow autoscaling (#88)
    * test environment
    
    Co-authored-by: Henrik <henrik>

--- terraform/production/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-project_id = "steam-airfoil-367315"
-region     = "europe-west8"
-zone       = "europe-west8-b"
--- terraform/production/versions.tf
+++ terraform/production/versions.tf
@@ -1,13 +1,5 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.27.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.7.1"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
